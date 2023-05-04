d94b1757ccb65d795d820c075ef2b518129992c1
cashmirB <128428924+cashmirB@users.noreply.github.com>
2023-05-04 22:31:57 +0100

    updated weather.js and css

--- src/Weather.js
+++ src/Weather.js
@@ -4,22 +4,19 @@ import "./Weather.css";
 
 export default function Weather() {
   return (
-    <div class="container">
-      <div class="weather-app-wrapper">
-        <div class="weather-app">
-          <form class="search-form">
-            <div class="row">
-              <div class="col-9">
+    <div className="container">
+      <div className="weather-app-wrapper">
+        <div className="weather-app">
+          <form className="search-form">
+            <div className="row">
+              <div className="col-9">
                 <input
                   type="search"
                   placeholder="Type a city.."
-                  autofocus="on"
-                  autocomplete="off"
                   class="form-control shadow-sm"
-                  id="city-input"
                 />
               </div>
-              <div class="col-3">
+              <div className="col-3">
                 <input
                   type="submit"
                   value="Search"
@@ -28,38 +25,34 @@ export default function Weather() {
               </div>
             </div>
           </form>
-          <h1 id="city">Sydney NSW, Australia</h1>
+          <h1>Sydney NSW, Australia</h1>
           <ul>
-            <li id="date">Wednesday 22:00</li>
-            <li id="description">Partly Cloudy</li>
+            <li>Wednesday 22:00</li>
+            <li>Partly Cloudy</li>
           </ul>
-          <div class="row">
-            <div class="col-9">
-              <div class="d-flex weather-temp">
+          <div className="row">
+            <div className="col-9">
+              <div className="d-flex weather-temp">
                 <img
                   src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
                   alt="Cloudy icon"
-                  id="icon"
                 />
 
-                <span class="temperature" id="currentTemp">
-                  19
-                </span>
-                <span class="units"> °C </span>
+                <span className="temperature">19</span>
+                <span className="units"> °C </span>
               </div>
             </div>
-            <div class="col-3">
+            <div className="col-3">
               <ul>
                 <li>
-                  Humidity: <span id="humidity">77</span>%
+                  Humidity: <span>77</span>%
                 </li>
                 <li>
-                  Wind: <span id="wind"> 8</span> m/h
+                  Wind: <span> 8</span> m/h
                 </li>
               </ul>
             </div>
           </div>
-          <div class="weather-forecast" id="forecast"></div>
         </div>
       </div>
     </div>
